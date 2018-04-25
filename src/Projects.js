daedcf230ea86591a3a85e0ecca7f0274d819140
brunoguazina <bruno@guazina.com>
2018-04-24 22:10:00 -0300

    update the loop in the tag component

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Tag from './Tag';
 
 import './Projects.scss';
--- src/Tag.js
+++ src/Tag.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
-
 class Tag extends Component {
-    
+
+    listarTag() {
+        
+        var tag = "";
+        const { data } = this.props;
+
+        for (const prop in data) {
+            console.log(`data.${prop} = ${data[prop]}`);
+            
+            tag = `${data[prop]}`;
+        }
+
+        return tag;
+    }
+
+
     render() {
 
-        
-
-        console.log(this.props.data);
+        //const {data} = this.props.data;
         
         return (
-            <span className="tag">{this.props.data.primeira}</span>
+            <span className="tag">{this.listarTag()}</span>
         );
     }
 }
