833ddd72f06fe8c2dc5c333d439001e987b177d9
brunoguazina <bruno@guazina.com>
2018-04-29 20:31:56 -0300

    including fonts/icons

--- src/Footer.js
+++ src/Footer.js
@@ -11,11 +11,12 @@ class Footer extends Component {
                     <h4>Like what you see?</h4>
                     <p className="footer-bold">Learn more <a href="/resume/">about me</a>, follow me or contact me below</p>
                     <ul className="clear">
-                        <li><a href="https://twitter.com/andrewcouldwell" className="icon-twitter">Twitter</a></li>
-                        <li><a href="https://dribbble.com/andrewcouldwell" className="icon-dribbble">Dribbble</a></li>
-                        <li><a href="https://medium.com/@andrewcouldwell" className="icon-medium">Medium</a></li>
-                        <li><a href="http://uk.linkedin.com/in/roomfive" className="icon-linkedin">LinkedIn</a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-behance">Behance</a></li>
+                        <li><a href="https://twitter.com/andrewcouldwell" className="icon-medium"> </a></li>
+                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-github"> </a></li>
+                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-linkedin"> </a></li>
+                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-youtube"> </a></li>
+                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-instagram"> </a></li>
+                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-facebook"> </a></li>
                     </ul>
                     <p className="email clear"><span className="maily"><a href="mailto:bruno@guazina.com">bruno@guazina.com</a></span></p>
                 </div>
