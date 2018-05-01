b42cf789fea57fc740a9df6b45e50268fbb8e484
brunoguazina <bruno@guazina.com>
2018-05-01 16:46:46 -0300

    adding url

--- src/Footer.js
+++ src/Footer.js
@@ -11,12 +11,12 @@ class Footer extends Component {
                     <h4>Like what you see?</h4>
                     <p className="footer-bold">Learn more <a href="/resume/">about me</a>, follow me or contact me below</p>
                     <ul className="clear">
-                        <li><a href="https://twitter.com/andrewcouldwell" className="icon-medium"> </a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-github"> </a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-linkedin"> </a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-youtube"> </a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-instagram"> </a></li>
-                        <li><a href="http://www.behance.net/andrewcouldwell" className="icon-facebook"> </a></li>
+                        <li><a href="https://medium.com/@brunoguazina" className="icon-medium"> </a></li>
+                        <li><a href="https://github.com/brunoguazina" className="icon-github"> </a></li>
+                        <li><a href="https://www.linkedin.com/in/bguazina/" className="icon-linkedin"> </a></li>
+                        <li><a href="https://www.youtube.com/channel/UCT35R6tsgcFTs_k_D9hzU-Q" className="icon-youtube"> </a></li>
+                        <li><a href="https://www.instagram.com/brunoguazina/" className="icon-instagram"> </a></li>
+                        <li><a href="https://www.facebook.com/brunoguazina/" className="icon-facebook"> </a></li>
                     </ul>
                     <p className="email clear"><span className="maily"><a href="mailto:bruno@guazina.com">bruno@guazina.com</a></span></p>
                 </div>
