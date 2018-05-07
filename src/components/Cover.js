4e5bca9342bc4f5b3b66e4c1464dacb62437e91c
brunoguazina <bruno@guazina.com>
2018-05-06 22:27:51 -0300

    Adjustments in icon learn more

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -5,11 +5,17 @@ class Cover extends Component {
     render() {
         return (
             <section id="cover">
+                
                 <div id="intro">
                     <h2> Bruno A. Guazina </h2>
                     <h1> User Experience Designer currently working at Magazine Luiza</h1>
                 </div>
-                <span className="icon-arrow_down icon"></span>    
+                
+                <div className="learnMore">
+                    <span className="txt">learn more</span>
+                    <span className="icon-arrow_down icon"></span>    
+                </div>
+
             </section>
         );
     }
