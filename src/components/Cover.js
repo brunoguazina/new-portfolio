3d895fa2118e1374f2e6bdb6fdeaf5bfe450a4b5
brunoguazina <bruno@guazina.com>
2018-05-07 22:07:52 -0300

    starting formatting in icon learn more

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+
 import './Cover.scss';
 
 class Cover extends Component {
+    
+
     render() {
+
         return (
             <section id="cover">
-                
+
                 <div id="intro">
                     <h2> Bruno A. Guazina </h2>
                     <h1> User Experience Designer currently working at Magazine Luiza</h1>
                 </div>
-                
-                <div className="learnMore">
+
+                <div id="learnMore">
+                <a href="#projects">
                     <span className="txt">learn more</span>
-                    <span className="icon-arrow_down icon"></span>    
+                    <span className="icon-arrow_down icon"></span>
+                </a>
                 </div>
 
             </section>
