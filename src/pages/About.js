d23a45f270382eeeeb01e9e44c564a42c6746a22
brunoguazina <bruno@guazina.com>
2018-05-09 07:07:20 -0300

    ajustement in compoment

--- src/pages/About.js
+++ src/pages/About.js
@@ -28,10 +28,14 @@ class About extends Component {
 
                     <div className="content"> 
                         <p>
-                            I’m UX/UI Designer & Product Designer from Brazil. I have more than 10 years of career, creating digital products for digital agencies, e-commerce, software house, and helping startups turn ideas into innovative digital products.
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
                         </p>
                         <p>
-                            I believe that access to information and technology creates a more just and inclusive world. That is why my goal is to identify people's problems and needs and to solve them by designing and implementing simple, creative, and objective solutions.
+                            I believe that access to information and technology creates a more just and inclusive world.
+                            That is why my goal is to identify people's problems and needs and to solve them by designing and
+                            implementing simple, creative, and objective solutions.
                         </p>
                         <p>
                             In addition, I live to travel and photograph.
