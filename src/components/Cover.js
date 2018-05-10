7a8dab22710dbb160903ce6428a154f1da7b91dd
brunoguazina <bruno@guazina.com>
2018-05-10 19:14:37 -0300

    adding script

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 import './Cover.scss';
 
 class Cover extends Component {
-    
+
+    tratar() {
+        return (window.addEventListener("scroll", function () {
+            var elemento = document.getElementById("learnMore");
+
+            if (window.scrollY > "50") {
+                
+                elemento.style.visibility = "hidden";
+            }
+            else elemento.style.visibility = "visible";
+        })
+        )
+    }
 
     render() {
 
@@ -15,11 +27,11 @@ class Cover extends Component {
                     <h1> User Experience Designer currently working at Magazine Luiza</h1>
                 </div>
 
-                <div id="learnMore">
-                <a href="#projects">
-                    <span className="txt">learn more</span>
-                    <span className="icon-arrow_down icon"></span>
-                </a>
+                <div id="learnMore" onLoad={this.tratar()}>
+                    <a href="#projects">
+                        <span className="txt">learn more</span>
+                        <span className="icon-arrow_down icon"></span>
+                    </a>
                 </div>
 
             </section>
