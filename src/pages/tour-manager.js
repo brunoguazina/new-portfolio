29e5f95555af709ecefa9e0c41f94c84538959ec
brunoguazina <bruno@guazina.com>
2018-05-10 19:15:16 -0300

    adding new page

--- src/pages/tour-manager.js
+++ src/pages/tour-manager.js
@@ -6,6 +6,11 @@ import Menu from './../components/Menu';
 import Footer from './../components/Footer';
 
 import chart from './../images/tour-manager/organization.png';
+import flow from './../images/tour-manager/flow.png';
+import ecosystem from './../images/tour-manager/ecosystem.png';
+import wireframe from './../images/tour-manager/wireframe.png';
+import mockup from './../images/tour-manager/mockup.png';
+
 import './about.scss';
 
 
@@ -26,7 +31,7 @@ class TourManager extends Component {
 
                     <h1> Tour Manager </h1>
 
-                    <div className="content"> 
+                    <div className="content">
                         <p>
                             I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
                             I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
@@ -40,6 +45,50 @@ class TourManager extends Component {
                         </p>
 
                         <img src={chart} alt="" />
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
+
+                        <img src={flow} alt="" />
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
+
+                        <img src={ecosystem} alt="" />
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
+
+                        <img src={wireframe} alt="" />
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
+
+                        <img src={mockup} alt="" />
+
+                        <p>
+                            I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
+                            I have more than 10 years of experience, creating digital products for digital agencies, e-commerce,
+                            software house, and helping startups turn ideas into innovative digital products.
+                        </p>
 
                         <p>
                             I’m Product Designer and UX/UI Designer from Brazil currently working at Magazine Luiza.
