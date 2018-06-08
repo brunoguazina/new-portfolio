535400fa10dc2e5fef728f0b54da1fe718ad50b2
brunoguazina <bruno@guazina.com>
2018-06-08 00:42:25 -0300

    adjustments

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class Tag extends Component {
 
-    listarProjetos(name, index) {
+    listar(name, index) {
         return <span key={index} className="tag"> {name}  </span>
     }
 
     render() {
         const { data } = this.props;
-        return data.map(this.listarProjetos)
+        
+        return data.map(this.listar)
     }
 }
 
